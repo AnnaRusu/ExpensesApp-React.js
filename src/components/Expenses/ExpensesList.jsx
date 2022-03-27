9cff4ed2645c4353cc989dfd822100f823e2b1cf
Ana-Maria Rusu <annelise.rusu@gmail.com>
2022-03-27 18:45:13 +0300

    feat: filter expenses

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -1,18 +1,39 @@
 import Card from "../UI/Card";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function ExpensesList({ items }) {
+  const [selectedValue, setSelectedValue] = useState("all");
+
+  const filteredExpenses = items.filter(expense => expense.date.getFullYear().toString() === selectedValue);
+  const renderAllExpenses = items.map(item => 
+    <ExpenseItem
+      key={item.id}
+      title={item.title}
+      price={item.price}
+      date={item.date}
+    />
+  )
+  const renderFilteredExpenses = filteredExpenses.map(item => 
+    <ExpenseItem
+      key={item.id}
+      title={item.title}
+      price={item.price}
+      date={item.date}
+    />
+  )
+
   return (
     <Card className="expenses">
-      {items.map((item) => (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          price={item.price}
-          date={item.date}
-        />
-      ))}
+      <ExpensesFilter
+        selected={selectedValue}
+        handleFilterChange={(value) => setSelectedValue(value)}
+        items={items}
+      />
+
+      {selectedValue === "all" ? renderAllExpenses : renderFilteredExpenses}
     </Card>
   );
 }
